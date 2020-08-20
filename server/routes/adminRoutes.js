5f52035a7a1a8be1a38dc008a28d116ee56a490a
parthnamdev <parthnamdev@users.noreply.github.com>
2020-08-20 19:39:39 +0530

    validated admin route

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+const { body, validator } = require('express-validator');
 
-router.get('/requestAccess', adminController.requestAccess);
+router.get('/requestAccess', [
+        body('admin','please enter valid admin username').notEmpty(),
+        body('password','please enter valid password').notEmpty()
+    ], adminController.requestAccess);
 router.post('/users', adminController.users);
 router.post('/artcles',adminController.articles);
 
