57bf6dd165929a7a3e7787d54f5e885a74eaf488
parthnamdev <parthnamdev@users.noreply.github.com>
2020-10-02 16:06:16 +0530

    fixing bugs

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,7 +4,7 @@ const Article = require('../models/articleModel');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, `${"uploads/" + req.body.username}`);
+        cb(null, `${"uploads/" + req.user.uuid}`);
     },
     filename: function(req, file, cb) {
         let ext = path.extname(file.originalname);
