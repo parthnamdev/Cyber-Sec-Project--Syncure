834dd925513e8c88bae17a38494adf3b1728653c
parthnamdev <parthnamdev@users.noreply.github.com>
2020-10-03 23:41:49 +0530

    testing

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,20 +3,20 @@ const passport = require('passport');
 
 const authenticate = (req, res, next) => {
     try {
-        if(req.isAuthenticated()){
+        // if(req.isAuthenticated()){
             const token = req.headers.authorization.split(' ')[1]
             const decode = jwt.verify(token, process.env.JWT_SECRET)
 
             req.user = decode
             next()
-        } else {
-            res.json({
-                status: "failure",
-                message: "unauthorised",
-                errors: [],
-                data: {}
-            })
-        }
+        // } else {
+        //     res.json({
+        //         status: "failure",
+        //         message: "unauthorised",
+        //         errors: [],
+        //         data: {}
+        //     })
+        // }
         
 
     } catch(err) {
